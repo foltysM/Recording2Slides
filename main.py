fa29b88894efb243956acac55aa23a63a3fc4d75
foltysM <michal@foltys.net>
2021-03-19 23:31:11 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import cv2 as cv
+import numpy as np
+
+
+cap = cv.VideoCapture('test2.mp4')
+i, last_i = 0, 0
+frame_list = []
+
+lower_black = np.array([0, 0, 0])  # HSV
+upper_black = np.array([180, 255, 30])
+
+ret, last_frame = cap.read()
+j = 0
+blacks = []
+greens = []
+
+cv.namedWindow("Preview", cv.WINDOW_KEEPRATIO)
+
+while True:
+    ret, frame30 = cap.read()
+    if not ret:
+        break
+
+    imr = cv.resize(frame30, (480, 270))
+    cv.imshow("Preview", imr)
+
+    diff = cv.absdiff(frame30, last_frame)
+    # cv.imshow("diff", diff)
+    last_frame = frame30
+
+    diff_hsv = cv.cvtColor(diff, cv.COLOR_BGR2HSV)
+
+    mask_black = cv.inRange(diff_hsv, lower_black, upper_black)
+    res = cv.bitwise_and(frame30, frame30, mask=mask_black)
+
+    black_percent = (mask_black > 0).mean()
+    print(black_percent)
+    if black_percent < 0.95 and j > 80:
+        cv.imwrite('slide' + str(j) + '.jpg', frame30)
+    j += 1
+    blacks.append(black_percent)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
